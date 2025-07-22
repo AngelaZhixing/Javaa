62af810b38e246001c2a2415dbe8189c1ff1f562
Angela Zhixing Lu <angelazhixing@gmail.com>
2025-07-22 15:09:29 -0700

    Resolved #4: Array Practice

---
+++ ArrayPractice.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class ArrayPractice {
+    public static void main(String[] args){
+        Random value = new Random();
+        int[] randomValues = new int[10];
+        for (int count = 0; count <10; count++){
+            int randomValue = value.nextInt(100) +1;
+            randomValues[count] = randomValue;
+            System.out.println(randomValue);
+        }
+        int length = randomValues.length;
+        int greatestNumber = randomValues[0];
+        for(int oneNumber : randomValues){
+            if (greatestNumber < oneNumber){
+                greatestNumber = oneNumber;
+            }
+        }
+        System.out.println("The greatest number is " +greatestNumber);
+    }
+}
